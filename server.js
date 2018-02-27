40982184cbca994ae5196821219eb660fa40cc38
Grillo, Nicolás Alfredo <nicolas.a.grillo@accenture.com>
2018-02-27 09:34:59 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var http = require('http');
+var fs = require('fs');
+
+var app = express();       
+
+fs.readFile('./dist/index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8080);
+});
