0db86ac7eee2b7a4acba1e82fd91d956a4072846
Nicolas Grillo <nicolasgrillo@live.com.ar>
2018-03-27 22:36:05 -0300

    Adding styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ls-root',
@@ -7,8 +8,14 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-	title = 'Libreria Segurola';
-	
-  constructor(){}
+  title = 'Libreria Segurola';
+  routeLinks:any[];
+  activeLinkIndex = 0;
+  constructor(private router: Router) {
+    this.routeLinks = [
+    {label: 'Home', link: '/home'},
+    {label: 'Excel', link: '/excel-import'}];
+
+  }
   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatIconModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { AppComponent } from './app.component';
 import { ExcelImportComponent } from './excel-import/excel-import.component';
@@ -16,7 +21,13 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatIconModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
