b83bd201fa7637c0196c2cc920889b24e7482c19
Grillo, Nicolás Alfredo <nicolas.a.grillo@accenture.com>
2018-03-31 12:48:29 -0300

    Tweaking reactive form add page

--- src/app/item-add/item-add.component.ts
+++ src/app/item-add/item-add.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators  } from '@angular/forms';
+import { Component, OnInit, ViewChild, QueryList, ElementRef, ViewChildren } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl  } from '@angular/forms';
 import { ItemService } from '../entities/item/item.service';
 import { Item } from '../entities/item/item';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-item-add',
@@ -10,34 +11,65 @@ import { Item } from '../entities/item/item';
 })
 export class ItemAddComponent implements OnInit {
 
+  @ViewChildren('isbnInput') isbnInput : QueryList<ElementRef>;
+
   bookForm : FormGroup;
   bookFound : Item;
-  newBook : Item;
+  newBook : Item = new Item();
   receiptCode : string;
-  datePicked: any = { date: { year: new Date().getFullYear() , month: new Date().getMonth() , day: new Date().getDate() } };
+  error: string;
+  datePicked: any;
   INgxMyDpOptions = {
-    // other options...
-    jsdate: new Date(),
     dateFormat: 'dd/mm/yyyy'
   };
   public showSpinner: boolean = false;
+
+  errorMsgIsbn : string;
+  errorMsgPublisher : string;
+  errorMsgPrice : string;
+  errorMsgTitle : string;
+  errorMsgReceipt : string;
+  errorMsgReturnDate : string;
+  validationMsg = 'The field is required';
   
   constructor(private fb: FormBuilder,
-    private itemService: ItemService) { 
-      this.createForm();
+    private itemService: ItemService,
+    private router: Router) {      
   }
 
   ngOnInit() {
+    this.createForm();
   }
 
-  ngOnChanges(){
-    this.rebuildForm();
-  }
+  ngAfterViewInit(): void {
+		if (!this.isbnInput.first) {
+			this.isbnInput.changes.subscribe(
+				(change: any) => this.focusInput()
+			)
+		}
+		else {
+			this.focusInput()
+		}
+	}
 
   onSubmit() {
-    this.newBook = this.prepareSaveBook();
-    this.itemService.saveItem(this.newBook).subscribe(/* error handling */);
-    this.rebuildForm();
+    if (this.bookForm.dirty && this.bookForm.valid){
+      this.newBook = this.prepareSaveBook();
+      this.error = null;
+      this.itemService.saveItem(this.newBook)
+        .finally(() => {
+          this.showSpinner = false;
+        })
+        .subscribe(
+          () =>  {
+            //Saved!
+            this.router.navigate(['/item-add'])
+            this.newBook = new Item();
+            this.createForm();
+          },
+          (err: any) => this.error = err
+        );
+    }
   }
 
   getBook(e){
@@ -46,8 +78,9 @@ export class ItemAddComponent implements OnInit {
       (respItem: Item) => {
         this.bookFound = respItem;
         this.newBook = respItem;
+        this.createForm();
         this.hideLoadingSpinner();
-        this.rebuildForm()
+        this.enableInputs()
       },
       error => {
         this.hideLoadingSpinner();
@@ -58,12 +91,12 @@ export class ItemAddComponent implements OnInit {
 
   createForm() {
     this.bookForm = this.fb.group({
-      isbn: ['', Validators.required ],
-      title: ['', Validators.required ],
-      publisher: ['', Validators.required ],
-      price: ['', Validators.required ],
-      receipt: ['', Validators.required ],
-      returnDate: ['', Validators.required ]
+      isbn: [this.newBook.isbn, [Validators.required]],
+      title: [this.newBook.title, [Validators.required]],
+      publisher: [this.newBook.publisher, [Validators.required]],
+      price: [this.newBook.price, [Validators.required]],
+      receipt: [this.newBook.receiptCode, [Validators.required]],
+      returnDate: [this.newBook.returnDate, [Validators.required]]
     });
 
     this.bookForm.get('title').disable();
@@ -71,17 +104,23 @@ export class ItemAddComponent implements OnInit {
     this.bookForm.get('price').disable();
     this.bookForm.get('receipt').disable();
     this.bookForm.get('returnDate').disable();
+
+    const isbnControl: AbstractControl = this.bookForm.get('isbn');
+    const titleControl: AbstractControl = this.bookForm.get('title');
+    const publisherControl: AbstractControl = this.bookForm.get('publisher');
+    const priceControl: AbstractControl = this.bookForm.get('price');
+    const receiptControl: AbstractControl = this.bookForm.get('receipt');
+    const returnDateControl: AbstractControl = this.bookForm.get('returnDate');
+
+    isbnControl.valueChanges.subscribe((v: any) => this.errorMsgIsbn = this.setMessage(isbnControl));
+    titleControl.valueChanges.subscribe((v: any) => this.errorMsgTitle = this.setMessage(titleControl));
+    publisherControl.valueChanges.subscribe((v: any) => this.errorMsgPublisher = this.setMessage(publisherControl));
+    priceControl.valueChanges.subscribe((v: any) => this.errorMsgPrice = this.setMessage(priceControl));
+    receiptControl.valueChanges.subscribe((v: any) => this.errorMsgReceipt = this.setMessage(receiptControl));
+    returnDateControl.valueChanges.subscribe((v: any) => this.errorMsgReturnDate = this.setMessage(returnDateControl));
   }
   
-  rebuildForm(){
-    this.bookForm = this.fb.group({
-      isbn: [this.bookFound.isbn , Validators.required],
-      title: [this.bookFound.title , Validators.required],
-      publisher: [this.bookFound.publisher , Validators.required],
-      price: [this.bookFound.price , Validators.required],
-      receipt: [this.receiptCode, Validators.required],
-      returnDate: [this.datePicked, Validators.required]       
-    });
+  enableInputs(){
     this.bookForm.get('title').enable();
     this.bookForm.get('publisher').enable();
     this.bookForm.get('price').enable();
@@ -89,8 +128,8 @@ export class ItemAddComponent implements OnInit {
     this.bookForm.get('returnDate').enable();
   }
 
-  prepareSaveBook() : Item{
-    
+  prepareSaveBook() : Item{    
+    this.showSpinner = true;
     const formModel = this.bookForm.value;
     var formattedDate = this.formatDatePicked(this.bookForm);
     var dateString = formattedDate.split('/');
@@ -115,6 +154,19 @@ export class ItemAddComponent implements OnInit {
     var yearPicked = bf.controls["returnDate"].value.date.year;
     return dayPicked + "/" + monthPicked +  "/" + yearPicked;
   }
+
+  setMessage(control: AbstractControl): string {
+		if ((control.touched || control.dirty) && control.errors) {
+			return Object.keys(control.errors).map(
+        (key: string) => this.validationMsg
+      ).join(' ');
+		}
+		return '';
+  }
+  
+  private focusInput(): void {
+		this.isbnInput.first.nativeElement.focus();
+	}
 
   showLoadingSpinner() {
     this.showSpinner = true;
